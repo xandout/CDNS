aaa3eb6a7bba36356ed878337437eb718140c559
Mitchell Turner <turnerdistributed@gmail.com>
2015-09-07 23:07:34 -0400

    ...

--- Server/cdns.h
+++ Server/cdns.h
@@ -1,34 +1,46 @@
 typedef struct {
     unsigned int ID : 16;
-    unsigned int QR : 1;
-    unsigned int OC : 4;
-    unsigned int AA : 1;
-    unsigned int TC : 1;
-    unsigned int RD : 1;
-    unsigned int RA : 1;
-    unsigned int Z : 1;
-    unsigned int AD : 1;
-    unsigned int CD : 1;
-    unsigned int RCODE : 4;
-    unsigned int QCOUNT : 16;
-    unsigned int ANSCOUNT : 16;
-    unsigned int AUTHCOUNT : 16;
-    unsigned int ARCOUNT : 16;
+    unsigned int QR : 1;            //00000001 1
+    unsigned int OC : 4;            //00011110 30
+    unsigned int AA : 1;            //00100000 32
+    unsigned int TC : 1;            //01000000 64
+    unsigned int RD : 1;            //10000000 128 End of B3
+    unsigned int RA : 1;            //00000001 1
+    unsigned int Z : 1;             //00000010 2
+    unsigned int AD : 1;            //00000100 4
+    unsigned int CD : 1;            //00001000 8
+    unsigned int RCODE : 4;         //11110000 240 End of B4
+    unsigned int QCOUNT : 16;       //B5
+    unsigned int ANSCOUNT : 16;     //B7
+    unsigned int AUTHCOUNT : 16;    //B9
+    unsigned int ARCOUNT : 16;      //B11
 } DNS_HEADER;
 
-
-
 DNS_HEADER* parse_header(char* packet, int packet_length)
-{
+{   //00000000
     DNS_HEADER* header = malloc(sizeof(DNS_HEADER));
-//    int x = 0;
-//    for(x = 0; x < 12; x++)
-//    {
-//        printf("%i", packet[x]);
-//    }
     header->ID = packet[0] << 8 | packet[1];
-    header->QR = ((packet[2] >> 1)  & 0x01); //Not certain this works yet
-    //header->OC = 
+    header->QR = ((packet[2] & 1)  >> 1); //Not certain this works yet
+    header->OC = ((packet[2] & 30)  >> 1);
+    header->AA = ((packet[2] & 32)  >> 1);
+    header->TC = ((packet[2] & 64)  >> 1);
+    header->RD = ((packet[2] & 128)  >> 1);
+    
+    header->RA = ((packet[3] & 1)  >> 1); 
+    header->Z = ((packet[3] & 2)  >> 1);
+    header->AD = ((packet[3] & 4)  >> 1);
+    header->CD = ((packet[3] & 8)  >> 1);
+    header->RCODE = ((packet[3] & 240)  >> 1);
+    
+    header->QCOUNT = packet[4] << 8 | packet[5];
+    header->ANSCOUNT = packet[6] << 8 | packet[7];
+    header->AUTHCOUNT = packet[8] << 8 | packet[9];
+    header->QCOUNT = packet[10] << 8 | packet[11];
+    
+    
+    
+    
+    
     return header;
 
 }
